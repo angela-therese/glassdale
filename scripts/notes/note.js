c582d0a18c436882a035c12f076e9a332801da0e
User-42 <a.t.iafrate@gmail.com>
2021-02-10 18:37:52 -0500

    criminal card contains criminalId, date, text

--- scripts/notes/note.js
+++ scripts/notes/note.js
@@ -2,7 +2,7 @@ export function noteEntry(formObject){
 
     return `
     <div class="notes-card">
-    <p><b>ID:</b> ${formObject.id}</p>    
+    <p><b>ID:</b> ${formObject.criminalId}</p>    
     <p><b>Date:</b> ${formObject.date}</p>
     <p><b>Entry:</b> ${formObject.text}</p>
     </div>
